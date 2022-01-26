d66f3e3ea1cbfa48a9bbd1b9e73619891a8396df
carlruan <kf_ruan981024@163.com>
2022-01-25 19:16:35 -0500

    node

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log("hi");
+
+
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const url = require("url");
+
+const server = http.createServer((req, res) => {
+    if(req.url == "/"){
+        fs.readFile(path.join(__dirname, "index.html"), (err, data) => {
+            res.writeHead(200, {"Content-Type" : "text/html"});
+            res.write(data);
+            res.end();
+        });
+    }else{
+        res.write("Hello");
+        res.end();
+    }
+});
+
+server.listen(3501, ()=>{console.log("3501 listening!")});
---
+++ try.js
@@ -0,0 +1,5 @@
+let morning = function(name){
+    console.log("good morning " + name);
+}
+
+exports.morning = morning;
