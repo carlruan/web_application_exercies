07e7535d0221eead1012a85e83a6a386010d98a6
carlruan <kf_ruan981024@163.com>
2022-01-25 19:49:20 -0500

    npm added

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 
 
-const http = require("http");
-const fs = require("fs");
-const path = require("path");
-const url = require("url");
+// const http = require("http");
+// const fs = require("fs");
+// const path = require("path");
+// const url = require("url");
 
-const server = http.createServer((req, res) => {
-    if(req.url == "/"){
-        fs.readFile(path.join(__dirname, "index.html"), (err, data) => {
-            res.writeHead(200, {"Content-Type" : "text/html"});
-            res.write(data);
-            res.end();
-        });
-    }else{
-        res.write("Hello");
-        res.end();
-    }
-});
+// const server = http.createServer((req, res) => {
+//     if(req.url == "/"){
+//         fs.readFile(path.join(__dirname, "index.html"), (err, data) => {
+//             res.writeHead(200, {"Content-Type" : "text/html"});
+//             res.write(data);
+//             res.end();
+//         });
+//     }else{
+//         res.write("Hello");
+//         res.end();
+//     }
+// });
 
-server.listen(3501, ()=>{console.log("3501 listening!")});
+// server.listen(3501, ()=>{console.log("3501 listening!")});
+
+const cowsay = require("cowsay");
+
+console.log(cowsay.say({
+    text : "I'm a cow",
+    e : "oO",
+    T : "U "
+}));
